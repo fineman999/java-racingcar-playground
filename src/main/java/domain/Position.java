b2f02644f803467c0efe014148b7adf55e1e7d2b
Chan <33cks1423@naver.com>
2023-07-20 15:53:04 +0900

    feat (name): Position 원시 값을 포장함

---
+++ src/main/java/domain/Position.java
@@ -0,0 +1,36 @@
+package domain;
+
+import java.util.Objects;
+
+public class Position {
+    private final int position;
+
+    public Position(int position) {
+        this.position = position;
+    }
+
+    public static Position startFromZero() {
+        return new Position(0);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "position=" + position +
+                '}';
+    }
+}
